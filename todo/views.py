8439d676e66d40a4a6938acc88b2f691a022e5b2
Bertin19 <juniorwagueu@gmail.com>
2021-04-30 01:26:45 +0200

    todo/views Retrieve, List

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
+
+# CRUD - Create, Retrieve, Update, Delete, List
+
+# List
+def todo_list(request):
+    todos = Todo.objects.all()
+    print("Test ", todos)
+    context = {
+        "todo_list": todos
+    }
+    return render(request, "todo/todo_list.html", context)
+
+# Retrieve
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        'todo': todo
+    }
+    return render(request, "todo/todo_detail.html", context)
